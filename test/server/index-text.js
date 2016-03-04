6f53286c28e0dfd16b9a3a7ec0bab6d09b4854d2
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-03-04 17:38:27 -0600

    Tests running

---
+++ test/server/index-text.js
@@ -0,0 +1,17 @@
+var request = require('supertest')
+var routes = require(__server + '/index.js')
+
+describe("The Server", function() {
+
+	//set up app
+	var app = TestHelper.createApp()
+	app.use('/', routes)
+	app.testReady()
+
+	//start tests!
+
+	it("returns true", function() {
+		expect(2===2).to.be.true
+	})
+
+})
---
+++ test/test-helper.js
@@ -0,0 +1,41 @@
+process.env.NODE_ENV = 'test'
+
+//Store the location of the server and client folders
+global.__server = __dirname + '/../server'
+global.__client = __dirname + '/../client'
+
+//Make chai's 'expect' accessible from everywhere
+var chai = require('chai')
+global.expect = chai.expect
+//Could instead make everything should-able:
+  // chai.should()
+
+//
+//Helper Functions
+//
+
+global.TestHelper = {};
+
+//
+//Exportable function setting up app for testing
+//
+
+var express = require('express')
+
+TestHelper.createApp = function(loader) {
+
+	var app = express()
+	app.use(require('body-parser').json())
+
+	app.testReady = function() {
+		//Log errors
+		app.use(function(err, req, res, next) {
+			console.error('==Error==')
+			console.error(' ' + err.stack)
+			next(err)
+		})
+	}
+
+	return app
+
+}
