937667ed71a51e6542017b4910c1ed62c37a9233
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-03-04 12:00:16 -0600

    table migration tentatively set up

--- migrations/20160303190431_med.js
+++ migrations/20160303190431_med.js
@@ -3,60 +3,38 @@ exports.up = function(knex, Promise) {
   
   return Promise.all([
 
-  	knex.schema.createTable('users', function(table) {
-  		table.increments('id_user').primary();
-  		table.string('username');
-  		table.string('password');
-  		table.string('email');
-  		table.timestamps();
-  	}),
+    knex.schema.createTable('users', function(table) {
+      table.increments('id_user').primary();
+      table.string('username');
+      table.string('password');
+      table.string('email');
+      table.timestamps();
+    }),
 
-  	knex.schema.createTable('doctors', function(table) {
-  		table.increments('id_doctor').primary();
-  		table.string('name');
-  		table.string('street_address');
-  		table.string('city');
-  		table.string('state_abbrev');
-  		table.integer('zip');
-  		table.string('email');
-  		table.string('web');
-  		table.string('phone');
-  		table.string('type');
-  	}),
+    knex.schema.createTable('doctors', function(table) {
+      table.increments('id_doctor').primary();
+      table.string('name');
+      table.string('street_address');
+      table.string('city');
+      table.string('state_abbrev');
+      table.integer('zip');
+      table.string('email');
+      table.string('web');
+      table.string('phone');
+      table.string('type');
+      table.timestamps();
+    }),
 
-  	knex.schema.createTable('user_doctor', function(table) {
-  		table.increments('id_user_doctor').primary();
-  	}
-  	  	 // knex.schema.createTable('users', function(table) {
-    //         table.increments('uid').primary();
-    //         table.string('username');
-    //         table.string('password');
-    //         table.string('name');
-    //         table.string('email');
-    //         table.timestamps();
-    //     }),
-
-    //     knex.schema.createTable('posts', function(table){
-    //         table.increments('id').primary();
-    //         table.string('title');
-    //         table.string('body');
-    //         table.integer('author_id')
-    //              .references('uid')
-    //              .inTable('users');
-    //         table.dateTime('postDate');
-    //     }),
-
-    //     knex.schema.createTable('comments', function(table){
-    //         table.increments('id').primary();
-    //         table.string('body');
-    //         table.integer('author_id')
-    //              .references('uid')
-    //              .inTable('users');
-    //         table.integer('post_id')
-    //              .references('id')
-    //              .inTable('posts');
-    //         table.dateTime('postDate');
-    //     })
+    knex.schema.createTable('user_doctor', function(table) {
+      table.increments('id_user_doctor').primary();
+      table.integer('id_user')
+           .references('id_user')
+           .inTable('users');
+      table.integer('id_doctor')
+           .references('id_doctor')
+           .inTable('doctors');
+      table.timestamps();
+    })
   ])
 
 };
@@ -65,8 +43,9 @@ exports.down = function(knex, Promise) {
   
 
   return Promise.all([
-  	    // knex.schema.dropTable('users'),
-       //  knex.schema.dropTable('posts'),
-       //  knex.schema.dropTable('comments')
+      knex.schema.dropTable('users'),
+      knex.schema.dropTable('doctors'),
+      knex.schema.dropTable('user_doctor')
   ])
+  
 };
