ec70e48de6041b38682a52b05824c0e197ebfd95
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-03-07 12:22:12 -0600

    able to write unique functions in models

--- server/models/model-helper.js
+++ server/models/model-helper.js
@@ -1,14 +1,3 @@
-// ===================================
-// Pseudocode
-// ===================================
-/*
-
-The model-helper should:
-	- 'Create' function, allowing you to create a new instance of the table specified
-	- takes attributes
-
-
-*/
 const db = require('../db')
 const Promise = require('bluebird')
 const _R = require('ramda')
@@ -32,42 +21,68 @@ module.exports = function(tableName, allAttrs) {
 	this.returnSuccess = _R.curry( function(description, result) {
 		console.log('inside reportSuccess')
 		console.log('***', description, '***')
-		console.log('result is', result)
-		return result[0]
+		// console.log('result is', result)
+
+		//NOTE: This returns an array of all results from the query, even if that array is length 1
+		//If you want to return a single object, add a .then statement after returnSuccess is called in your function
+		//(see this.create below for an example)
+		return result
+
 	}).bind(this)
 
 
     //Creates a new entry using attribute object passed in
+    //Returns the new object created
 	this.create = function(newItemAttrs) {
-		console.log('inside create', newItemAttrs)
+		// console.log('trying to create user', newItemAttrs)
 		return db(this.table).insert(newItemAttrs, this.attrsToSet)
-		  .then(this.returnSuccess('successfully created new entry into table ' + this.table))
-		  .catch(this.reportError('error inserting into table ' + this.table))
+		  .then( this.returnSuccess('successfully created new entry into table ' + this.table) )
+		  .then( function(result) { return result[0] })
+		  .catch( this.reportError('error inserting into table ' + this.table) )
 	}.bind(this)
 
+	//Returns an array of all rows in the table
 	this.getAll = function() {
+		// console.log('trying to get all users')
 		return db.select('*').from(this.table)
-		  .then(this.returnSuccess('success getting all entries in ' + this.table))
-		  .catch(this.reportError('error getting all entries from ' + this.table))
+		  .then( this.returnSuccess('success getting all entries in ' + this.table) )
+		  .catch( this.reportError('error getting all entries from ' + this.table) )
 	}.bind(this)
 
-	//Returns all rows from the table matching the id passed in
+	//Returns an object from the table where the primary key matches the id passed in
+	//If the entry does not exist, returns undefined
 	this.findById = function(id) {
 		var queryObj = {}
 		queryObj[this.idVarName] = id
 
 		return db.select('*').from(this.table).where(queryObj)
-		  .then(this.returnSuccess('success in retrieving from' + this.table))
-		  .catch(this.reportError('error finding by id from ' + this.table))
+		  .then( this.returnSuccess('success in retrieving from' + this.table) )
+		  .then( function(result) { return result[0] })
+		  .catch( this.reportError('error finding by id from ' + this.table) )
 	}.bind(this)
 
+	//Generic function to quickly create searches by a single attribute
+	  //Returns an array of all matching elements
+	//Set the keyName inside a the specific model, eg User.findByUsername = this.findByAttribute('username')
+	  //Then the function will take one argument, value, which will be eg the username you are seraching for
+	this.findByAttribute = _R.curry(function(keyName, value) {
+		var queryObj = {}
+		queryObj[keyName] = value
+		console.log('queryObj in findByAttribute', queryObj)
+
+		return db.select('*').from(this.table).where(queryObj)
+		.then( this.returnSuccess('success in retrieving from' + this.table) )
+		.catch( this.reportError('error finding by id from ' + this.table) ) 
+	}).bind(this)
+
 	//Deletes entry from table
+	//Returns the number of records deleted (ie 1)
 	this.deleteById = function(id) {
 		var queryObj = {}
 		queryObj[this.idVarName] = id
 
 		return db(this.table).where(queryObj).del()
-		  .then(this.returnSuccess('success deleting from' + this.table))
+		  .then(this.returnSuccess('success deleting from ' + this.table))
 		  .catch(this.reportError('error deleting from ' + this.table))
 	}.bind(this)
 
--- server/models/user.js
+++ server/models/user.js
@@ -3,3 +3,15 @@ const Model = require('./model-helper')
 
 const User = new Model('users', ['id_user', 'username', 'password', 'email', 'phone'])
 module.exports = User
+
+//User.create(attrs)
+
+User.findByUsername = function(username) {
+	return this.findByAttribute('username', username)
+	.then( function(userArray) { console.log('got userArray', userArray); return userArray[0] })
+}
+
+User.findByEmail = function(email) {
+	return this.findByAttribute('email', email)
+	.then( function(userArray) { console.log('got userArray', userArray); return userArray[0] })
+}
