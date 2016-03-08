dce3e96ed6efa7327e4408e1ac50d37e4618d4df
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-03-08 16:49:31 -0600

    refactored bcrypt to use bcrypt-node

--- server/models/user.js
+++ server/models/user.js
@@ -1,145 +1,150 @@
-// const db = require('../db')
-// const Model = require('./model-helper')
-// const Promise = require('bluebird')
-// const bcrypt = require('bcrypt-as-promised')
+const db = require('../db')
+const Model = require('./model-helper')
+const Promise = require('bluebird')
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
 
-// const User = new Model('users', ['id_user', 'username', 'password', 'email', 'phone'])
-// module.exports = User
+const User = new Model('users', ['id_user', 'username', 'password', 'email', 'phone'])
+module.exports = User
 
-// // METHODS DEFIEND IN MODEL-HELPER
+// METHODS DEFIEND IN MODEL-HELPER
 
-// /* 
-// User.create = function(attrs)
-//   Takes attributes of new user and adds this user to the users table
-//   Returns the newly created entry
-//   For some reason, the timestamp etc is not working
-//   eg:
-//     User.create( {username: 'example_user', password: 'example_pw', email: 'sample_email', phone: '111-111-1111'} )
-//     => returns: {id_user: somenumber,
-//                  username: 'example_user', 
-//                  password: 'example_pw', 
-//                  email: 'sample_email', 
-//                  phone: '111-111-1111',
-//                  created_at: null,
-//                  updated_at: null
-//                 }
-// */
-
-
-// /* User.getAll = function()
-//   Returns an array of all users
-// */
+/* 
+User.create = function(attrs)
+  Takes attributes of new user and adds this user to the users table
+  Returns the newly created entry
+  For some reason, the timestamp etc is not working
+  eg:
+    User.create( {username: 'example_user', password: 'example_pw', email: 'sample_email', phone: '111-111-1111'} )
+    => returns: {id_user: somenumber,
+                 username: 'example_user', 
+                 password: 'example_pw', 
+                 email: 'sample_email', 
+                 phone: '111-111-1111',
+                 created_at: null,
+                 updated_at: null
+                }
+*/
 
 
-// /*
-// User.findById = function(id)
-//   Takes a primary key as an argument
-//   Returns an object containing the user with a matching primary key, or undefined if no entry has a matching primary key
-//   eg:
-//     User.findById(7)
-//     => returns: {id_user: 7,
-//                  username: 'seventh_user,
-//                  password: 'example_pw', 
-//                  email: 'sample_email', 
-//                  phone: '111-111-1111',
-//                  created_at: null,
-//                  updated_at: null
-//                 }
-// */
-
-// /* 
-// User.deleteById = function(id)
-//   Takes a primary key as an argument
-//   Deletes the user with that primary key from the database
-//   Returns the number of records deleted (1)
-// */
+/* User.getAll = function()
+  Returns an array of all users
+*/
 
 
-// //METHODS DEFINED HERE:
+/*
+User.findById = function(id)
+  Takes a primary key as an argument
+  Returns an object containing the user with a matching primary key, or undefined if no entry has a matching primary key
+  eg:
+    User.findById(7)
+    => returns: {id_user: 7,
+                 username: 'seventh_user,
+                 password: 'example_pw', 
+                 email: 'sample_email', 
+                 phone: '111-111-1111',
+                 created_at: null,
+                 updated_at: null
+                }
+*/
 
-// /* 
-//   This is like the generic Model.create, but hashes the password before storing it
-// */
-// User.createUser = function(attrs) {
-//   return bcrypt.hash(attrs.password, 10)
-//     .then( function(hash) {
-//       var newUserObj = attrs
-//       newUserObj.password = hash
-//       return User.create(newUserObj)
-//     })
-//     .catch( function(err) {
-//       console.log('error hashing password', err)
-//     })
-// }
-
-
-//  Compares passed-in password to the stored hash
-//   eg: If Alice's password is 'mynameisnotbob' hashes to 'ajt894gjoq4tgjao', then:
-//     User.passwordMatches('mynameisnotbob', 'ajt894gjoq4tgjao')
-//       => returns true
-//     User.passwordMatches('alicesfakepassword, 'ajt894gjoq4tgjao')
-//       => returns false
-
-// User.passwordMatches = function(enteredPw, storedHash) {
-//   return bcrypt.compare(enteredPw, storedHash)
-//     .catch( function(error) {
-//       return error instanceof bcrypt.MISMATCH_ERROR ? false : error
-//     })
-// }
+/* 
+User.deleteById = function(id)
+  Takes a primary key as an argument
+  Deletes the user with that primary key from the database
+  Returns the number of records deleted (1)
+*/
 
 
-// /* eg: User.findByUsername('bob')
-//   => returns: 
-//                    {id_user: 82,
-//                    username: 'bob'
-//                    password: 'bobspassword',
-//                    email: 'bob@bobcat.com',
-//                    phone: '111-111-1111',
-//                    created_at: null,
-//                    updated_at: null
-//                   }
-//     returns undefined if there is no user with username 'bob'
-// */
-// User.findByUsername = function(username) {
-// 	return this.findByAttribute('username', username)
-// 	.then( function(userArray) { return userArray[0] })
-// }
+//METHODS DEFINED HERE:
+
+/* 
+  This is like the generic Model.create, but hashes the password before storing it
+*/
+User.createUser = function(attrs) {
+  return bcrypt.genSaltAsync(10)
+    .then( function(salt) {
+      console.log('got salt', salt)
+      return bcrypt.hashAsync(attrs.password, salt, null)
+    })
+    .then( function(hash) {
+      console.log('hash', hash)
+      var newUserObj = attrs
+      newUserObj.password = hash
+      return User.create(newUserObj)
+    })
+    .catch( function(err) {
+      console.log('error hashing password', err)
+    })
+}
+
+/*
+ Compares passed-in password to the stored hash
+  eg: If Alice's password is 'mynameisnotbob' hashes to 'ajt894gjoq4tgjao', then:
+    User.passwordMatches('mynameisnotbob', 'ajt894gjoq4tgjao')
+      => returns true
+    User.passwordMatches('alicesfakepassword, 'ajt894gjoq4tgjao')
+      => returns false
+*/
+User.passwordMatches = function(enteredPw, storedHash) {
+  return bcrypt.compareAsync(enteredPw, storedHash)
+    .catch( function(error) {
+      return error instanceof bcrypt.MISMATCH_ERROR ? false : error
+    })
+}
 
 
-
-// /* eg: User.findByEmail('bob@bobcat.com')
-//   => returns: 
-//                    {id_user: 82,
-//                    username: 'bob'
-//                    password: 'bobspassword',
-//                    email: 'bob@bobcat.com',
-//                    phone: '111-111-1111',
-//                    created_at: null,
-//                    updated_at: null
-//                   }
-//     returns undefined if there is no user with email 'bob@bobcat.com'
-// */
-// User.findByEmail = function(email) {
-// 	return this.findByAttribute('email', email)
-// 	.then( function(userArray) { console.log('got userArray', userArray); return userArray[0] })
-// }
+/* eg: User.findByUsername('bob')
+  => returns: 
+                   {id_user: 82,
+                   username: 'bob'
+                   password: 'bobspassword',
+                   email: 'bob@bobcat.com',
+                   phone: '111-111-1111',
+                   created_at: null,
+                   updated_at: null
+                  }
+    returns undefined if there is no user with username 'bob'
+*/
+User.findByUsername = function(username) {
+	return this.findByAttribute('username', username)
+	.then( function(userArray) { return userArray[0] })
+}
 
 
 
-// /*
-//   Returns a boolean indicating whether the passed-in password matches the password on record
-// */
-// User.validPassword = function(username, password) {
-//   return this.findByUsername(username)
-//     .then( function(userInfo) {
-//       return User.passwordMatches(password, userInfo.password)
-//     })
-// }
+/* eg: User.findByEmail('bob@bobcat.com')
+  => returns: 
+                   {id_user: 82,
+                   username: 'bob'
+                   password: 'bobspassword',
+                   email: 'bob@bobcat.com',
+                   phone: '111-111-1111',
+                   created_at: null,
+                   updated_at: null
+                  }
+    returns undefined if there is no user with email 'bob@bobcat.com'
+*/
+User.findByEmail = function(email) {
+	return this.findByAttribute('email', email)
+	.then( function(userArray) { console.log('got userArray', userArray); return userArray[0] })
+}
 
-// /*
-//   Returns a boolean indicating whether the username exists in the database
-// */
-// User.existsByUsername = function(username) {
-//   return this.existsByAttribute('username', username)
-// }
+
+
+/*
+  Returns a boolean indicating whether the passed-in password matches the password on record
+*/
+User.validPassword = function(username, password) {
+  return this.findByUsername(username)
+    .then( function(userInfo) {
+      return User.passwordMatches(password, userInfo.password)
+    })
+}
+
+/*
+  Returns a boolean indicating whether the username exists in the database
+*/
+User.existsByUsername = function(username) {
+  return this.existsByAttribute('username', username)
+}
 
--- test/server/models_test/user_test.js
+++ test/server/models_test/user_test.js
@@ -1,253 +1,253 @@
-// require(TEST_HELPER)
+require(TEST_HELPER)
 
-// const db = require(__server + '/db')
-// const request = require('supertest-as-promised')
+const db = require(__server + '/db')
+const request = require('supertest-as-promised')
 
-// const User = require(__server + '/models/user')
-
-
-// describe('User Model', function() {
-
-// 	beforeEach(function() {
-// 		return db.deleteEverything()
-// 	})
+const User = require(__server + '/models/user')
 
 
-// 	var UserAttributes = function(username, password, email, phone) {
-// 		this.username = username
-// 		this.password = password
-// 		this.email = email
-// 		this.phone = phone
-// 	}
+describe('User Model', function() {
 
-// 	it('inserts user', function () {
-
-// 		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
-
-// 		return User.createUser(newTestUser)
-// 		  .then(function(user) {
-// 		  	// console.log('got back from create', user)
-// 		  	expect(user.username).to.equal(newTestUser.username)
-// 		  })
-// 	})
-
-// 	it('retrieves all user data', function() {
-
-// 		var newTestUser1 = new UserAttributes('bob1', 'alice1', 'bob1@alice1.com', '123-789-3456')
-// 		var newTestUser2 = new UserAttributes('bob2', 'alice2', 'bob2@alice2.com', '123-789-3456')
-
-// 		return User.createUser(newTestUser1)
-// 		  .then( function() { return User.create(newTestUser2) })
-// 		  .then( function() { return User.getAll() })
-// 		  .then( function(allUsers) {
-// 		  	// console.log('got all users: ', allUsers)
-// 		  	expect(allUsers).to.have.length(2)
-// 		  	expect(allUsers[0]['username']).to.equal('bob1')
-// 		  	expect(allUsers[0]['email']).to.equal('bob1@alice1.com')
-// 		  	expect(allUsers[0]['phone']).to.equal('123-789-3456')
-// 		  	expect(allUsers[1]['username']).to.equal('bob2')
-// 		  	expect(allUsers[1]['email']).to.equal('bob2@alice2.com')
-// 		  	expect(allUsers[1]['phone']).to.equal('123-789-3456')
-
-// 		  	return User.passwordMatches('alice1', allUsers[0]['password'])
-// 		  })
-// 		  .then( function(result) {
-// 		  	expect(result).to.be.true
-// 		  })
-// 	})
-
-// 	it('retrieves a user by id', function() {
-
-// 		//these will be assigned after the db entries are created
-// 		var testId1 = undefined
-// 		var testId2 = undefined
-
-// 		var newTestUser3 = new UserAttributes('bob3', 'alice3', 'bob3@alice3.com', '123-789-3456')
-// 		var newTestUser4 = new UserAttributes('bob4', 'alice4', 'bob4@alice4.com', '123-789-3456')
-
-// 		return User.createUser(newTestUser3)
-// 		  .then( function() { return User.createUser(newTestUser4) })
-// 		  .then( function() { return User.getAll() })
-// 		  .then( function(allUsers) {
-
-// 		  	//set id variables to test
-// 		  	testId1 = allUsers[0]['id_user']
-// 		  	testId2 = allUsers[1]['id_user']
-
-// 		  	return User.findById(testId1)
-// 		  })
-// 		  .then( function(result) {
-// 		  	// console.log('got', result, 'by id')
-// 		  	expect(result.id_user).to.equal(testId1)
-// 		  	expect(result.username).to.equal('bob3')
-// 		  	// expect(result.password).to.equal('alice3')
-// 		  	expect(result.email).to.equal('bob3@alice3.com')
-// 		  	expect(result.phone).to.equal('123-789-3456')
-// 		  	return User.passwordMatches('alice3', result.password)
-// 		 })
-// 		.then( function(result) {
-// 			expect(result).to.be.true
-// 			return User.findById(testId2)
-// 		})
-// 		.then( function(result) {
-// 		  	// console.log('got', result, 'by id')
-// 		  	expect(result.id_user).to.equal(testId2)
-// 		  	expect(result.username).to.equal('bob4')
-// 		  	expect(result.email).to.equal('bob4@alice4.com')
-// 		  	expect(result.phone).to.equal('123-789-3456')
-
-// 		  	return User.passwordMatches('alice4', result.password)
-// 		})
-// 		.then( function(result) {
-// 			expect(result).to.be.true
-// 		})
-// 	})
-
-// 	it('deletes a user', function() {
+	beforeEach(function() {
+		return db.deleteEverything()
+	})
 
 
-// 		//this will be assigned after the db entries are created
-// 		var testId = undefined
+	var UserAttributes = function(username, password, email, phone) {
+		this.username = username
+		this.password = password
+		this.email = email
+		this.phone = phone
+	}
 
-// 		var newTestUser5 = new UserAttributes('bob5', 'alice5', 'bob5@alice5.com', '123-789-3456')
-// 		var newTestUser6 = new UserAttributes('bob6', 'alice6', 'bob6@alice6.com', '123-789-3456')
+	it('inserts user', function () {
 
-// 		return User.createUser(newTestUser5)
-// 		  .then( function() { return User.createUser(newTestUser6) })
-// 		  .then( function() { return User.getAll() })
-// 		  .then( function(allUsers) {
+		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
 
-// 		  	//set id variable to test
-// 		  	testId = allUsers[0]['id_user']
+		return User.createUser(newTestUser)
+		  .then(function(user) {
+		  	// console.log('got back from create', user)
+		  	expect(user.username).to.equal(newTestUser.username)
+		  })
+	})
 
-// 		  	return User.deleteById(testId)
-// 		  })
-// 		  .then( function(recordsDeleted) {
-// 		  	expect(recordsDeleted).to.equal(1)
+	it('retrieves all user data', function() {
 
-// 		  	return User.findById(testId)
-// 		  })
-// 		  .then(function(deletedRecord) {
-// 		  	expect(deletedRecord).to.be.an('undefined')
+		var newTestUser1 = new UserAttributes('bob1', 'alice1', 'bob1@alice1.com', '123-789-3456')
+		var newTestUser2 = new UserAttributes('bob2', 'alice2', 'bob2@alice2.com', '123-789-3456')
 
-// 		  	return User.getAll()
-// 		  })
-// 		  .then( function(allUsers) {
-// 		  	expect(allUsers).to.have.length(1)
-// 		  	expect(allUsers[0]['username']).to.equal('bob6')
-// 		  	expect(allUsers[0]['email']).to.equal('bob6@alice6.com')
-// 		  	expect(allUsers[0]['phone']).to.equal('123-789-3456')
-// 		  })
+		return User.createUser(newTestUser1)
+		  .then( function() { return User.create(newTestUser2) })
+		  .then( function() { return User.getAll() })
+		  .then( function(allUsers) {
+		  	// console.log('got all users: ', allUsers)
+		  	expect(allUsers).to.have.length(2)
+		  	expect(allUsers[0]['username']).to.equal('bob1')
+		  	expect(allUsers[0]['email']).to.equal('bob1@alice1.com')
+		  	expect(allUsers[0]['phone']).to.equal('123-789-3456')
+		  	expect(allUsers[1]['username']).to.equal('bob2')
+		  	expect(allUsers[1]['email']).to.equal('bob2@alice2.com')
+		  	expect(allUsers[1]['phone']).to.equal('123-789-3456')
 
-// 	})
+		  	return User.passwordMatches('alice1', allUsers[0]['password'])
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.true
+		  })
+	})
 
-// 	it('finds users by username', function() {
+	it('retrieves a user by id', function() {
 
-// 		var newTestUser7 = new UserAttributes('bob7', 'alice7', 'bob7@alice7.com', '123-789-3456')
-// 		var newTestUser8 = new UserAttributes('bob8', 'alice8', 'bob8@alice8.com', '123-789-3456')
+		//these will be assigned after the db entries are created
+		var testId1 = undefined
+		var testId2 = undefined
 
-// 		return User.createUser(newTestUser7)
-// 		  .then( function() { return User.createUser(newTestUser8) })
-// 		  .then( function() {
-// 		  	return User.findByUsername('bob8')
-// 		  })
-// 		  .then( function(user) {
-// 		  	expect(user).to.be.an('object')
-// 		  	expect(user).to.have.any.keys('id_user', 'username', 'password', 'email', 'phone')
-// 		  	expect(user.username).to.equal('bob8')
-// 		  	expect(user.email).to.equal('bob8@alice8.com')
-// 		  	expect(user.phone).to.equal('123-789-3456')
+		var newTestUser3 = new UserAttributes('bob3', 'alice3', 'bob3@alice3.com', '123-789-3456')
+		var newTestUser4 = new UserAttributes('bob4', 'alice4', 'bob4@alice4.com', '123-789-3456')
 
-// 		  	return User.passwordMatches('alice8', user.password)
-// 		  })
-// 		  .then(function(result) {
-// 		  	expect(result).to.be.true
-// 		  })
+		return User.createUser(newTestUser3)
+		  .then( function() { return User.createUser(newTestUser4) })
+		  .then( function() { return User.getAll() })
+		  .then( function(allUsers) {
 
-// 	})
+		  	//set id variables to test
+		  	testId1 = allUsers[0]['id_user']
+		  	testId2 = allUsers[1]['id_user']
 
-// 	it('finds users by email', function() {
+		  	return User.findById(testId1)
+		  })
+		  .then( function(result) {
+		  	// console.log('got', result, 'by id')
+		  	expect(result.id_user).to.equal(testId1)
+		  	expect(result.username).to.equal('bob3')
+		  	// expect(result.password).to.equal('alice3')
+		  	expect(result.email).to.equal('bob3@alice3.com')
+		  	expect(result.phone).to.equal('123-789-3456')
+		  	return User.passwordMatches('alice3', result.password)
+		 })
+		.then( function(result) {
+			expect(result).to.be.true
+			return User.findById(testId2)
+		})
+		.then( function(result) {
+		  	// console.log('got', result, 'by id')
+		  	expect(result.id_user).to.equal(testId2)
+		  	expect(result.username).to.equal('bob4')
+		  	expect(result.email).to.equal('bob4@alice4.com')
+		  	expect(result.phone).to.equal('123-789-3456')
 
-// 		var newTestUser9 = new UserAttributes('bob9', 'alice9', 'bob9@alice9.com', '123-789-3456')
-// 		var newTestUser10 = new UserAttributes('bob10', 'alice10', 'bob10@alice10.com', '123-789-3456')
+		  	return User.passwordMatches('alice4', result.password)
+		})
+		.then( function(result) {
+			expect(result).to.be.true
+		})
+	})
 
-// 		return User.createUser(newTestUser9)
-// 		  .then( function() { return User.createUser(newTestUser10) })
-// 		  .then( function() {
-// 		  	return User.findByEmail('bob10@alice10.com')
-// 		  })
-// 		  .then( function(user) {
-// 		  	expect(user).to.be.an('object')
-// 		  	expect(user).to.have.any.keys('id_user', 'username', 'password', 'email', 'phone')
-// 		  	expect(user.username).to.equal('bob10')
-// 		  	expect(user.email).to.equal('bob10@alice10.com')
-// 		  	expect(user.phone).to.equal('123-789-3456')
+	it('deletes a user', function() {
 
-// 		  	return User.passwordMatches('alice10', user.password)
-// 		  })
-// 		  .then( function(result) {
-// 		  	expect(result).to.be.true
-// 		  })
-// 	})
 
-// 	it('validates passwords', function() {
+		//this will be assigned after the db entries are created
+		var testId = undefined
 
-// 		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
+		var newTestUser5 = new UserAttributes('bob5', 'alice5', 'bob5@alice5.com', '123-789-3456')
+		var newTestUser6 = new UserAttributes('bob6', 'alice6', 'bob6@alice6.com', '123-789-3456')
 
-// 		return User.createUser(newTestUser)
-// 		  .then( function(user) {
-// 		  	return User.validPassword('bob', 'alice')
-// 		  })
-// 		  .then( function(result) {
-// 		  	expect(result).to.be.true
+		return User.createUser(newTestUser5)
+		  .then( function() { return User.createUser(newTestUser6) })
+		  .then( function() { return User.getAll() })
+		  .then( function(allUsers) {
 
-// 		  	return User.validPassword('bob', 'alice2')
-// 		  })
-// 		  .then( function(result) {
-// 		  	expect(result).to.be.false
-// 		  })
-// 	})
+		  	//set id variable to test
+		  	testId = allUsers[0]['id_user']
 
-// 	it('checks if a user exists', function() {
+		  	return User.deleteById(testId)
+		  })
+		  .then( function(recordsDeleted) {
+		  	expect(recordsDeleted).to.equal(1)
 
-// 		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
+		  	return User.findById(testId)
+		  })
+		  .then(function(deletedRecord) {
+		  	expect(deletedRecord).to.be.an('undefined')
 
-// 		return User.createUser(newTestUser)
-// 		  .then( function() {
-// 		  	return User.existsByUsername('bob')
-// 		  })
-// 		  .then( function(result) {
-// 		  	expect(result).to.be.true
+		  	return User.getAll()
+		  })
+		  .then( function(allUsers) {
+		  	expect(allUsers).to.have.length(1)
+		  	expect(allUsers[0]['username']).to.equal('bob6')
+		  	expect(allUsers[0]['email']).to.equal('bob6@alice6.com')
+		  	expect(allUsers[0]['phone']).to.equal('123-789-3456')
+		  })
 
-// 		  	return User.existsByUsername('alice')
-// 		  })
-// 		  .then( function(result) {
-// 		  	expect(result).to.be.false
-// 		  })
-// 	})
+	})
 
-// 	it('salts a password and adds that to the db', function() {
+	it('finds users by username', function() {
 
-// 		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
-// 		var hashedPw = undefined
-// 		var correctPw = 'alice'
-// 		var incorrectPw = 'Alice'
+		var newTestUser7 = new UserAttributes('bob7', 'alice7', 'bob7@alice7.com', '123-789-3456')
+		var newTestUser8 = new UserAttributes('bob8', 'alice8', 'bob8@alice8.com', '123-789-3456')
 
-// 		return User.createUser(newTestUser)
-// 		  .then(function(user) {
-// 		  	console.log('created user', user)
-// 		  	hashedPw = user.password
+		return User.createUser(newTestUser7)
+		  .then( function() { return User.createUser(newTestUser8) })
+		  .then( function() {
+		  	return User.findByUsername('bob8')
+		  })
+		  .then( function(user) {
+		  	expect(user).to.be.an('object')
+		  	expect(user).to.have.any.keys('id_user', 'username', 'password', 'email', 'phone')
+		  	expect(user.username).to.equal('bob8')
+		  	expect(user.email).to.equal('bob8@alice8.com')
+		  	expect(user.phone).to.equal('123-789-3456')
+
+		  	return User.passwordMatches('alice8', user.password)
+		  })
+		  .then(function(result) {
+		  	expect(result).to.be.true
+		  })
+
+	})
+
+	it('finds users by email', function() {
+
+		var newTestUser9 = new UserAttributes('bob9', 'alice9', 'bob9@alice9.com', '123-789-3456')
+		var newTestUser10 = new UserAttributes('bob10', 'alice10', 'bob10@alice10.com', '123-789-3456')
+
+		return User.createUser(newTestUser9)
+		  .then( function() { return User.createUser(newTestUser10) })
+		  .then( function() {
+		  	return User.findByEmail('bob10@alice10.com')
+		  })
+		  .then( function(user) {
+		  	expect(user).to.be.an('object')
+		  	expect(user).to.have.any.keys('id_user', 'username', 'password', 'email', 'phone')
+		  	expect(user.username).to.equal('bob10')
+		  	expect(user.email).to.equal('bob10@alice10.com')
+		  	expect(user.phone).to.equal('123-789-3456')
+
+		  	return User.passwordMatches('alice10', user.password)
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.true
+		  })
+	})
+
+	it('validates passwords', function() {
+
+		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
+
+		return User.createUser(newTestUser)
+		  .then( function(user) {
+		  	return User.validPassword('bob', 'alice')
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.true
+
+		  	return User.validPassword('bob', 'alice2')
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.false
+		  })
+	})
+
+	it('checks if a user exists', function() {
+
+		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
+
+		return User.createUser(newTestUser)
+		  .then( function() {
+		  	return User.existsByUsername('bob')
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.true
+
+		  	return User.existsByUsername('alice')
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.false
+		  })
+	})
+
+	it('salts a password and adds that to the db', function() {
+
+		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
+		var hashedPw = undefined
+		var correctPw = 'alice'
+		var incorrectPw = 'Alice'
+
+		return User.createUser(newTestUser)
+		  .then(function(user) {
+		  	console.log('created user', user)
+		  	hashedPw = user.password
 		  	
-// 		  	return User.passwordMatches(correctPw, hashedPw)
-// 		  })
-// 		  .then(function(result) {
-// 		  	expect(result).to.be.true
+		  	return User.passwordMatches(correctPw, hashedPw)
+		  })
+		  .then(function(result) {
+		  	expect(result).to.be.true
 
-// 		  	return User.passwordMatches(incorrectPw, hashedPw)
-// 		  })
-// 		  .then(function(result) {
-// 		  	expect(result).to.be.false
-// 		  })
-// 	})
+		  	return User.passwordMatches(incorrectPw, hashedPw)
+		  })
+		  .then(function(result) {
+		  	expect(result).to.be.false
+		  })
+	})
 
-// })
+})
