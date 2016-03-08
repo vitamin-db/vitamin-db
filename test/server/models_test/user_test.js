319d5dac5507f665d7d3ee70f51509ac2ffcca82
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-03-07 23:08:26 -0600

    adding bcrypt to User model

--- test/server/models_test/user_test.js
+++ test/server/models_test/user_test.js
@@ -24,7 +24,7 @@ describe('User Model', function() {
 
 		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
 
-		return User.create(newTestUser)
+		return User.createUser(newTestUser)
 		  .then(function(user) {
 		  	// console.log('got back from create', user)
 		  	expect(user.username).to.equal(newTestUser.username)
@@ -36,20 +36,23 @@ describe('User Model', function() {
 		var newTestUser1 = new UserAttributes('bob1', 'alice1', 'bob1@alice1.com', '123-789-3456')
 		var newTestUser2 = new UserAttributes('bob2', 'alice2', 'bob2@alice2.com', '123-789-3456')
 
-		return User.create(newTestUser1)
+		return User.createUser(newTestUser1)
 		  .then( function() { return User.create(newTestUser2) })
 		  .then( function() { return User.getAll() })
 		  .then( function(allUsers) {
 		  	// console.log('got all users: ', allUsers)
 		  	expect(allUsers).to.have.length(2)
 		  	expect(allUsers[0]['username']).to.equal('bob1')
-		  	expect(allUsers[0]['password']).to.equal('alice1')
 		  	expect(allUsers[0]['email']).to.equal('bob1@alice1.com')
 		  	expect(allUsers[0]['phone']).to.equal('123-789-3456')
 		  	expect(allUsers[1]['username']).to.equal('bob2')
-		  	expect(allUsers[1]['password']).to.equal('alice2')
 		  	expect(allUsers[1]['email']).to.equal('bob2@alice2.com')
 		  	expect(allUsers[1]['phone']).to.equal('123-789-3456')
+
+		  	return User.passwordMatches('alice1', allUsers[0]['password'])
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.true
 		  })
 	})
 
@@ -62,8 +65,8 @@ describe('User Model', function() {
 		var newTestUser3 = new UserAttributes('bob3', 'alice3', 'bob3@alice3.com', '123-789-3456')
 		var newTestUser4 = new UserAttributes('bob4', 'alice4', 'bob4@alice4.com', '123-789-3456')
 
-		return User.create(newTestUser3)
-		  .then( function() { return User.create(newTestUser4) })
+		return User.createUser(newTestUser3)
+		  .then( function() { return User.createUser(newTestUser4) })
 		  .then( function() { return User.getAll() })
 		  .then( function(allUsers) {
 
@@ -77,21 +80,27 @@ describe('User Model', function() {
 		  	// console.log('got', result, 'by id')
 		  	expect(result.id_user).to.equal(testId1)
 		  	expect(result.username).to.equal('bob3')
-		  	expect(result.password).to.equal('alice3')
+		  	// expect(result.password).to.equal('alice3')
 		  	expect(result.email).to.equal('bob3@alice3.com')
 		  	expect(result.phone).to.equal('123-789-3456')
-
-		  	return User.findById(testId2)
-		  })
-		  .then( function(result) {
+		  	return User.passwordMatches('alice3', result.password)
+		 })
+		.then( function(result) {
+			expect(result).to.be.true
+			return User.findById(testId2)
+		})
+		.then( function(result) {
 		  	// console.log('got', result, 'by id')
 		  	expect(result.id_user).to.equal(testId2)
 		  	expect(result.username).to.equal('bob4')
-		  	expect(result.password).to.equal('alice4')
 		  	expect(result.email).to.equal('bob4@alice4.com')
 		  	expect(result.phone).to.equal('123-789-3456')
-		  })
 
+		  	return User.passwordMatches('alice4', result.password)
+		})
+		.then( function(result) {
+			expect(result).to.be.true
+		})
 	})
 
 	it('deletes a user', function() {
@@ -103,8 +112,8 @@ describe('User Model', function() {
 		var newTestUser5 = new UserAttributes('bob5', 'alice5', 'bob5@alice5.com', '123-789-3456')
 		var newTestUser6 = new UserAttributes('bob6', 'alice6', 'bob6@alice6.com', '123-789-3456')
 
-		return User.create(newTestUser5)
-		  .then( function() { return User.create(newTestUser6) })
+		return User.createUser(newTestUser5)
+		  .then( function() { return User.createUser(newTestUser6) })
 		  .then( function() { return User.getAll() })
 		  .then( function(allUsers) {
 
@@ -126,7 +135,6 @@ describe('User Model', function() {
 		  .then( function(allUsers) {
 		  	expect(allUsers).to.have.length(1)
 		  	expect(allUsers[0]['username']).to.equal('bob6')
-		  	expect(allUsers[0]['password']).to.equal('alice6')
 		  	expect(allUsers[0]['email']).to.equal('bob6@alice6.com')
 		  	expect(allUsers[0]['phone']).to.equal('123-789-3456')
 		  })
@@ -138,8 +146,8 @@ describe('User Model', function() {
 		var newTestUser7 = new UserAttributes('bob7', 'alice7', 'bob7@alice7.com', '123-789-3456')
 		var newTestUser8 = new UserAttributes('bob8', 'alice8', 'bob8@alice8.com', '123-789-3456')
 
-		return User.create(newTestUser7)
-		  .then( function() { return User.create(newTestUser8) })
+		return User.createUser(newTestUser7)
+		  .then( function() { return User.createUser(newTestUser8) })
 		  .then( function() {
 		  	return User.findByUsername('bob8')
 		  })
@@ -147,9 +155,13 @@ describe('User Model', function() {
 		  	expect(user).to.be.an('object')
 		  	expect(user).to.have.any.keys('id_user', 'username', 'password', 'email', 'phone')
 		  	expect(user.username).to.equal('bob8')
-		  	expect(user.password).to.equal('alice8')
 		  	expect(user.email).to.equal('bob8@alice8.com')
 		  	expect(user.phone).to.equal('123-789-3456')
+
+		  	return User.passwordMatches('alice8', user.password)
+		  })
+		  .then(function(result) {
+		  	expect(result).to.be.true
 		  })
 
 	})
@@ -159,8 +171,8 @@ describe('User Model', function() {
 		var newTestUser9 = new UserAttributes('bob9', 'alice9', 'bob9@alice9.com', '123-789-3456')
 		var newTestUser10 = new UserAttributes('bob10', 'alice10', 'bob10@alice10.com', '123-789-3456')
 
-		return User.create(newTestUser9)
-		  .then( function() { return User.create(newTestUser10) })
+		return User.createUser(newTestUser9)
+		  .then( function() { return User.createUser(newTestUser10) })
 		  .then( function() {
 		  	return User.findByEmail('bob10@alice10.com')
 		  })
@@ -168,11 +180,14 @@ describe('User Model', function() {
 		  	expect(user).to.be.an('object')
 		  	expect(user).to.have.any.keys('id_user', 'username', 'password', 'email', 'phone')
 		  	expect(user.username).to.equal('bob10')
-		  	expect(user.password).to.equal('alice10')
 		  	expect(user.email).to.equal('bob10@alice10.com')
 		  	expect(user.phone).to.equal('123-789-3456')
-		  })
 
+		  	return User.passwordMatches('alice10', user.password)
+		  })
+		  .then( function(result) {
+		  	expect(result).to.be.true
+		  })
 	})
 
 	it('validates passwords', function() {
@@ -180,7 +195,7 @@ describe('User Model', function() {
 		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
 		var userObj = undefined
 
-		return User.create(newTestUser)
+		return User.createUser(newTestUser)
 		  .then( function(user) {
 		  	userObj = user
 
@@ -194,6 +209,34 @@ describe('User Model', function() {
 		  .then( function(result) {
 		  	expect(result).to.be.false
 		  })
+	})
+
+	it('salts a password and adds that to the db', function() {
+
+		var newTestUser = new UserAttributes('bob', 'alice', 'bob@alice.com', '123-789-3456')
+		var hashedPw = undefined
+		var correctPw = 'alice'
+		var incorrectPw = 'Alice'
+
+		return User.createUser(newTestUser)
+		  .then(function(user) {
+		  	console.log('created user', user)
+		  	hashedPw = user.password
+		  	
+		  	return User.passwordMatches(correctPw, hashedPw)
+		  })
+		  .then(function(result) {
+		  	console.log('should be true', result)
+		  	expect(result).to.be.true
+
+		  	return User.passwordMatches(incorrectPw, hashedPw)
+		  })
+		  .then(function(result) {
+		  	console.log('should be false', result)
+		  	expect(result).to.be.false
+		  })
+
+
 	})
 
 })
