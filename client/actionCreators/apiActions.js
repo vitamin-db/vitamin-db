93c601051b7bb610382e4f0ee0070c9c26d4e9a5
Joel Ahn <joelahn123@yahoo.com>
2016-03-09 16:26:38 -0600

    add ajax request

--- client/actionCreators/apiActions.js
+++ client/actionCreators/apiActions.js
@@ -1,1 +1,22 @@
-actions.js
+$ = jQuery = require('jquery');
+
+const Request = {
+  SignIn: (body) => {
+  	return $.ajax({
+  	  url: 'http://httpbin.org/post',
+  	  method: 'POST',
+  	  data: JSON.stringify(body),
+  	  dataType: 'json',
+  	  contentType: 'application/json',
+  	  success: (data) => {
+  	  	const purse = JSON.parse(data.data);
+  	  	console.log(purse);
+  	  },
+  	  error: (err) => {
+  	  	console.error(err)
+  	  }
+  	});
+  }
+};
+
+module.exports = Request;
--- client/containers/SplashView.js
+++ client/containers/SplashView.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const connect = require('react-redux').connect;
 const SignIn = require('../components/Splash/SignIn');
+const action = require('../actionCreators/apiActions');
 
 const Splash = ({ onSignIn }) => {
 	return (
@@ -21,7 +22,11 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		onSignIn: (e) => {
 			e.preventDefault();
-			console.log("HELLO")
+			console.log("this", e.target.username.value)
+			var username = e.target.username.value;
+			var password = e.target.password.value;
+			var body = {username: username, password: password};
+			dispatch(action.SignIn(body));
 		}
 	};
 };
