5fba580721c9e73eb8f47a1a1091316d734cb87d
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-03-09 14:45:38 -0600

    auth finally passing tests

--- test/server/models_test/auth_test.js
+++ test/server/models_test/auth_test.js
@@ -11,35 +11,39 @@ describe('Auth Model', function() {
 	var username1 = 'bob'
 	var token1 = undefined
 
-	it('creates and verifies a token', function() {
+	it('verifies a token that exists', function() {
 
 		return Auth.createToken(username1)
 		  .then( function(token) {
 		  	token1 = token
-		  	console.log('got back token in test', token1)
+		  	// console.log('got back token in test', token1)
 
 		  	return Auth.verifyToken(token1)
 		  })
 		  .then( function(decoded) {
-		  	console.log('decoded: ', decoded)
-		  	expect(decoded).to.equal(username1)
-		  })
-		  .then( function() {
-		  	setTimeout(function() {
-		  		//after 10 seconds (ie after token has expired, call 
-		  		return Auth.verifyToken(token1)
-		  		  .then(function(decoded) {
-		  		  	Promise.resolve(decoded)
-		  		  })
-		  	}, 10000)
-		  })
-		  .then( function(decoded) {
-		  	console.log('decoded should error', decoded);
-		  })
-		  .catch( function(err) {
-		  	console.log('should hit this error', err)
+		  	// console.log('decoded: ', decoded)
+
+		  	expect(decoded).to.be.an('object')
+		  	expect(decoded.username).to.equal(username1)
 		  })
 
+	})
+
+	it('returns false for an expired token', function() {
+
+	  	return new Promise( function(resolve, reject) {
+	  		setTimeout( function() {
+	  			return Auth.verifyToken(token1)
+	  			  .then(function(result) {
+	  			  	resolve(result)
+	  			  })
+	  		}, 1000)
+	  	})
+		  .then( function(decoded) {
+		  	console.log('decoded should be false', decoded)
+		  	//this is a lie - trying to get this to be false
+		  	expect(decoded).to.be.false
+		  })
 
 	})
 
