0d901740659cb23ad2f4ff2153261762ad542b17
Joel Ahn <joelahn123@yahoo.com>
2016-03-10 15:17:49 -0600

    uncommented out index.js stuff

--- client/store/Store.js
+++ client/store/Store.js
@@ -5,7 +5,8 @@ const reducer = require('../reducers/reducer');
 // create a store with the combined reducers as the argument
 // this connects the two. Simply return the created store to use in the Provider route
 module.exports = () => {
-  const finalCreateStore = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
-  const store = finalCreateStore(reducer);
+  // const finalCreateStore = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
+  // const store = finalCreateStore(reducer);
+  const store = createStore(reducer);
   return store;
 };
