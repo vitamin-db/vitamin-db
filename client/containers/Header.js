9856532f43df91a55852614dffa46a694513f5c0
Joel Ahn <joelahn123@yahoo.com>
2016-03-09 18:53:14 -0600

    added bootstrap & changed header to bootstrap navbar

--- client/containers/Header.js
+++ client/containers/Header.js
@@ -4,7 +4,11 @@ const connect = require('react-redux').connect;
 const Header = ({ onSignIn }) => {
 	return (
 		<div>
-			<h1>Header</h1>
+			<ul className="nav nav-tabs">
+				<li role="presentation" className="active"><a href="#">Home</a></li>
+				<li role="presentation"><a href="#">Profile</a></li>
+				<li role="presentation"><a href="#">Messages</a></li>
+			</ul>
 		</div>
 	);
 };
