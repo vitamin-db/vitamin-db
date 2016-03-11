f380a7ffaafefb7cb1d0d3c6a53898050046c297
Amberley Romo <amberleyjohanna@gmail.com>
2016-03-11 11:45:51 -0600

    adding login actions

--- client/actionCreators/apiActions.js
+++ client/actionCreators/apiActions.js
@@ -2,21 +2,43 @@ $ = jQuery = require('jquery');
 const stateAction = require('./stateActions');
 // This file is for all client api requests
 // this request below is just a test request for now
+
+// function SignIn (body) {
+//   return $.ajax({
+//     url: 'http://httpbin.org/post',
+//     method: 'POST',
+//     data: JSON.stringify(body),
+//     accepts: 'application/json',
+//     dataType: 'json',
+//     contentType: 'application/json',
+//     success: (data) => {
+//       const purse = JSON.parse(data.data);
+//       console.log(purse);
+//     },
+//     error: (err) => {
+//       console.error(err)
+//     }
+//   });
+// }
+
 function SignIn (body) {
-  return $.ajax({
-    url: 'http://httpbin.org/post',
-    method: 'POST',
-    data: JSON.stringify(body),
-    accepts: 'application/json',
-    dataType: 'json',
-    contentType: 'application/json',
-    success: (data) => {
-      const purse = JSON.parse(data.data);
-      console.log(purse);
+  fetch('/authenticate/login', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
-    error: (err) => {
-      console.error(err)
-    }
+    body: JSON.stringify(body)
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(j) {
+    console.log('j: ', j);
+    window.localStorage.setItem("token", j['token']);
+  })
+  .catch(function(err) {
+    console.error(err)
   });
 }
 
@@ -44,3 +66,4 @@ module.exports = {
   SignIn,
   SignUp
 };
+
