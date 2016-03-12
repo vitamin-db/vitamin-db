91d1cf2becdbb55bbe0c8e95b617e79581fc2fe7
Joel Ahn <joelahn123@yahoo.com>
2016-03-12 15:40:28 -0600

    modularized Doctor list & added a card list file

--- client/components/HomePage/PanelGrid.js
+++ client/components/HomePage/PanelGrid.js
@@ -5,36 +5,17 @@ const Grid = require('react-bootstrap').Grid;
 const Panel = require('react-bootstrap').Panel;
 const Button = require('react-bootstrap').Button;
 const Glyphicon = require('react-bootstrap').Glyphicon;
+const DoCard = require('./DoctorCard');
+const AddDoc = require('./AddDoctor');
 
 const PanelGrid = ({docInfo}) => (
 <div className="container-fluid">
   <Grid>
     <Row className="show-grid">
-
-    {docInfo.map((val) => 
-      <Col xs={4} md={4}>
-         <div className="card">
-         <div className="card-header">
-         <Button bsStyle="success" bsSize="small" className="card-button"><Glyphicon glyph={val.glyph} /></Button>
-          <h5 className="card-type">{val.type}</h5>
-         </div>
-         <div className="card-block">
-         <p className="card-text"><b>Name</b>: {val.name}</p>
-         <p className="card-text"><b>Address</b>: {val.street_address}</p>
-         <p className="card-text"><b>Phone</b>: {val.phone}</p>
-         </div>
-        </div>
-      </Col>
+      {docInfo.map((val) => 
+        <DoCard key={val.name} val={val} />
       )}
-      <Col xs={4} md={4}>
-      <div className="card">
-        <div className="card-header">
-          <Button bsStyle="Primary" bsSize="small" onClick={this.open}><Glyphicon glyph="plus" /></Button>
-          <h5 className="card-type">Add Doctor</h5>
-        </div>
-      </div>
-      </Col>
-
+      <AddDoc />
     </Row>
   </Grid>
   </div>
