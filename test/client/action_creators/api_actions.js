3ac00f01a434b3fdb9b2821809f89817c6f5155c
Joel Ahn <joelahn123@yahoo.com>
2016-03-13 01:41:45 -0600

    minor syntax changes

--- test/client/action_creators/api_actions.js
+++ test/client/action_creators/api_actions.js
@@ -3,7 +3,6 @@ const apiAction = require(__client + '/actionCreators/apiActions');
 
 describe('Calling API action: Sign Up', function(){
 	it('Should return a dispatch function', function(){
-
 		expect(apiAction.SignUp()).to.be.an('function')
 	})
 })
--- test/client/action_creators/state_actions.js
+++ test/client/action_creators/state_actions.js
@@ -6,35 +6,28 @@ const stateAction = require(__client + '/actionCreators/stateActions');
 
 describe('Calling state action: Sign Up Submit', function(){
 	it('should return the correct action type property', function() {
-
 	  	expect(stateAction.SignUpSubmit().type).to.equal('SUBMIT');
 	})
 
 	it('should return the correct action info property', function(){
-
 		var testData = {username: "username", password: "password", email: "email", phone: "phone"};
-
 		expect(stateAction.SignUpSubmit(testData).info).to.equal(testData)
 	})
 })
 
 describe('Calling state action: Sign in success', function(){
 	it('should return the correct action type property', function() {
-
 	  	expect(stateAction.SignInSuccess().type).to.equal('SIGNINSUCCESS');
 	})
 
 	it('should return the correct action token property', function(){
-
 		var testToken = "abc123";
-
 		expect(stateAction.SignInSuccess(testToken).token).to.equal(testToken)
 	})
 })
 
 describe('Calling state action: Sign in fail', function(){
 	it('should return the correct action type property', function() {
-
 	  	expect(stateAction.SignInFail().type).to.equal('SIGNINFAIL');
 	})
 })
