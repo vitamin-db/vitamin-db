06e7fa429b5ad02bb6a9735aa7e25067348799e0
Joel Ahn <joelahn123@yahoo.com>
2016-03-13 00:35:32 -0600

    rebasing: minor changes

--- test/client/action_creators/state_actions.js
+++ test/client/action_creators/state_actions.js
@@ -10,7 +10,7 @@ describe('Calling all state action: Sign Up Submit', function(){
 	})
 })
 
-describe('Calling all state action-creators', function(){
+describe('Calling all state action: Sign in success', function(){
 	it('should return the correct type property', function() {
 
 	  	expect(stateAction.SignUpSubmit().type).to.equal('SUBMIT');
@@ -20,7 +20,7 @@ describe('Calling all state action-creators', function(){
 	})
 })
 
-describe('Calling all state action-creators', function(){
+describe('Calling all state action: Sign in fail', function(){
 	it('should return the correct type property', function() {
 
 	  	expect(stateAction.SignUpSubmit().type).to.equal('SUBMIT');
