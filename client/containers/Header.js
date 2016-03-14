b454303577a4c60e44a0fe3ea0c2782db092638f
Joel Ahn <joelahn123@yahoo.com>
2016-03-14 18:45:36 -0500

    minor changes

--- client/containers/Header.js
+++ client/containers/Header.js
@@ -5,16 +5,16 @@ const connect = require('react-redux').connect;
 const apiAction = require('../actionCreators/apiActions');
 const stateAction = require('../actionCreators/stateActions');
 
-const Header = ({ goHome, signOut }) => {
+const Header = ({ goHome, signOut, goProfile, goAppoint }) => {
 	return (
 	  <nav className="navbar navbar-default">
         <div className="container-fluid">
           <ul className="nav nav-tabs">
           	  <li><h3>LOGO</h3></li>
-	          <li role="presentation"><button onClick={goHome}>Home</button></li>
-	          <li role="presentation"><button >Profile</button></li>
-	          <li role="presentation"><button >Appointments</button></li>
-	          <li role="presentation"><button onClick={signOut} >Sign out</button></li>
+	          <li role="presentation">{apiAction.isAuth() && <button onClick={goHome}>Home</button>}</li>
+	          <li role="presentation">{apiAction.isAuth() && <button onClick={goProfile} >Profile</button>}</li>
+	          <li role="presentation">{apiAction.isAuth() && <button onClick={goAppoint} >Appointments</button>}</li>
+	          <li role="presentation">{apiAction.isAuth() && <button onClick={signOut} >Sign out</button>}</li>
           </ul>
         </div> 
 	  </nav>
@@ -33,6 +33,20 @@ const mapDispatchToProps = (dispatch) => {
 		goHome: () => {
 			if(apiAction.isAuth()){
 				location.assign('/home')
+			}else{
+				console.log("FAIL")
+			}
+		},
+		goProfile: () => {
+			if(apiAction.isAuth()){
+				location.assign('/profile')
+			}else{
+				console.log("FAIL")
+			}
+		},
+		goAppoint: () => {
+			if(apiAction.isAuth()){
+				location.assign('/appointments')
 			}else{
 				console.log("FAIL")
 			}
