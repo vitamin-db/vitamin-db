e89f973cd1ae1ac920a9cda5b7bb0c5d8c16ff13
Joel Ahn <joelahn123@yahoo.com>
2016-03-15 14:41:14 -0500

    fixed header links if/else statements

--- client/containers/ProfileView.js
+++ client/containers/ProfileView.js
@@ -4,9 +4,6 @@ const connect = require('react-redux').connect;
 const Profiler = require('../components/User/Profile');
 
 const Profile = ({info}) => {
-  if(!window.localStorage.getItem("token")){
-    location.assign('/')
-  }
   return (
     <div>
       <h1>User's Profile view</h1>
