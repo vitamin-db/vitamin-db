9e3c07998e04083c49e71240dc64ef0b6e5ead89
Joel Ahn <joelahn123@yahoo.com>
2016-03-15 14:11:43 -0500

    oops, commented out all of signup reduce

--- test/client/reducers/sign_up.js
+++ test/client/reducers/sign_up.js
@@ -1,20 +1,20 @@
-require(TEST_HELPER)
-const SignupReducer = require(__client + '/reducers/SignUp');
-const stateAction = require(__client + '/actionCreators/stateActions');
+// require(TEST_HELPER)
+// const SignupReducer = require(__client + '/reducers/SignUp');
+// const stateAction = require(__client + '/actionCreators/stateActions');
 
-describe('Calling Sign up reducer initially', function(){
-	it('Should return the correct intial state username property', function(){
-		expect(SignupReducer().username).to.equal("username")
-	})
+// describe('Calling Sign up reducer initially', function(){
+// 	it('Should return the correct intial state username property', function(){
+// 		expect(SignupReducer().username).to.equal("username")
+// 	})
 
-	it('Should return the correct initial state email property', function(){
-		expect(SignupReducer().email).to.equal("email")
-	})
+// 	it('Should return the correct initial state email property', function(){
+// 		expect(SignupReducer().email).to.equal("email")
+// 	})
 
-	it('Should return the correct intial state phone property', function(){
-		expect(SignupReducer().phone).to.equal("phone")
-	})
-})
+// 	it('Should return the correct intial state phone property', function(){
+// 		expect(SignupReducer().phone).to.equal("phone")
+// 	})
+// })
 
 // describe("Calling Sign up with action type: 'SUBMIT'", function(){
 // 	var testInfo = { username: "Joellama", email: "asdf@yahoo.com", phone: 123};
