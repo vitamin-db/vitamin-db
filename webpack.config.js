5287e993eb0c97e8d9fcfa9dd8fda0021c7810e4
Daniel Dinh <dtoand@yahoo.com>
2016-03-15 15:31:21 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+// var webpack = require('webpack');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
- entry: [
-    'webpack-hot-middleware/client',
-    './client/app.js'
-  ],
-  output: {
-        path: __dirname + "/dist",
-        filename: "app-bundle.js"
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['react', 'es2015', 'react-hmre']
-        }
-      }
-    ]
-  },
-    devServer: {
-    hot: true
-  }
-}
+// module.exports = {
+//  entry: [
+//     'webpack-hot-middleware/client',
+//     './client/app.js'
+//   ],
+//   output: {
+//         path: __dirname + "/dist",
+//         filename: "app-bundle.js"
+//   },
+//   plugins: [
+//     new webpack.optimize.OccurrenceOrderPlugin(),
+//     new webpack.HotModuleReplacementPlugin(),
+//     new webpack.NoErrorsPlugin(),
+//   ],
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js$/,
+//         loader: 'babel-loader',
+//         exclude: /node_modules/,
+//         query: {
+//           presets: ['react', 'es2015', 'react-hmre']
+//         }
+//       }
+//     ]
+//   },
+//     devServer: {
+//     hot: true
+//   }
+// }
+
+
+
+
 // module.exports = {
 //   entry: [
 //     'webpack-dev-server/client?http://localhost:8080',
