b0c1101eddd26ac558a8e2614cb4093c812f1d0d
Joel Ahn <joelahn123@yahoo.com>
2016-03-15 23:18:11 -0500

    fixed up reducers a bit

--- client/reducers/DoctorList.js
+++ client/reducers/DoctorList.js
@@ -1,14 +1,16 @@
-// const DocList = (state, action) => {
-//   // state will be an object with: username, email, phone
-//   if(state === undefined){
-//     return state = [];
-//   }
-//   switch(action.type){
-//     case 'GETLIST':
-//       return state = action.list
-//     default:
-//       return state;
-//   }
-// };
+const DocList = (state, action) => {
+  // state will be an array with object doctors. Expected doctor object data = {username, email, phone}
+  if(state === undefined){
+    return state = [];
+  }
+  switch(action.type){
+    case 'GETLIST':
+    	return action.list; // server will return an array filled with doctor objects, return it and it will be set to the state variable
+    case 'ADDLIST':
+    	return [...state, action.doctor]; // ask server to return the newly added doctor and make a new array with doctor objects in it
+    default:
+      return state;
+  }
+};
 
-// module.exports = DocList;
+module.exports = DocList;
