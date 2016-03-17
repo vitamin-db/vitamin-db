186b43a26b07cf4fbeb7f8bd04c170e7bc38161d
Daniel Dinh <dtoand@yahoo.com>
2016-03-16 23:36:36 -0500

    Update PatientGrid.js

--- client/components/Dashboard/patient/PatientGrid.js
+++ client/components/Dashboard/patient/PatientGrid.js
@@ -1,6 +1,6 @@
 const React = require('react');
 // import React from 'react';
-const _ = require('lodash');
+// const _ = require('lodash');
 const Row = require('react-bootstrap').Row;
 const Grid = require('react-bootstrap').Grid;
 // const AddInfo = require('./AddInfo');
