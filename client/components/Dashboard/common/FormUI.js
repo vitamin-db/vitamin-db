0cbc154b5c12b6bd97b813c6c42aa6db76212ab3
Daniel Dinh <dtoand@yahoo.com>
2016-03-18 13:49:11 -0500

    material UI style form

---
+++ client/components/Dashboard/common/FormUI.js
@@ -0,0 +1,12 @@
+const React = require('react');
+const TextField = require ('material-ui/lib/text-field');
+
+const TextFieldEx = () => (
+  <div>
+    <TextField
+      hintText="Hint Text"
+    />
+  </div>
+);
+
+module.exports = TextFieldEx;
