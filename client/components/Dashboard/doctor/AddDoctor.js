dd6c964b4b3444b0c8f5f0e6ec1d580780ac7191
Daniel Dinh <dtoand@yahoo.com>
2016-03-18 13:53:39 -0500

    add doc component

--- client/components/Dashboard/doctor/AddDoctor.js
+++ client/components/Dashboard/doctor/AddDoctor.js
@@ -1,26 +1,11 @@
 const React = require('react');
 const Row = require('react-bootstrap').Row;
 const Col = require('react-bootstrap').Col;
-const Grid = require('react-bootstrap').Grid;
-const Panel = require('react-bootstrap').Panel;
 const Button = require('react-bootstrap').Button;
 const Glyphicon = require('react-bootstrap').Glyphicon;
-const ButtonToolbar = require('react-bootstrap').ButtonToolbar;
 const Modal = require('react-bootstrap').Modal;
 const Input = require('react-bootstrap').Input;
 const FormComponent = require('./Form');
-
-
-// const AddForm = () => {
-// <div>
-//   <form className="form-horizontal">
-//     <Input type="text" label="Text" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-//     <Input type="textarea" label="Textarea" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-//     <Input type="checkbox" label="Checkbox" wrapperClassName="col-xs-offset-2 col-xs-10" help="Offset is applied to wrapper." />
-//   </form>
-//   </div>
-//  )
-// }
 
 
 const AddDoc = React.createClass({
@@ -36,7 +21,7 @@ getInitialState() {
   render() {
 	return(
     <div>
-      <ButtonToolbar>
+
         <Col xs={4} md={4}>
         <div className="card">
           <div className="card-header">
@@ -55,7 +40,6 @@ getInitialState() {
 
         </Modal>
 
-      </ButtonToolbar>
     </div>
     )
   }
