2dc5ef9193de0d0e9ec3228911edfd2825b86bfb
Amberley Romo <amberleyjohanna@gmail.com>
2016-03-21 17:12:03 -0500

    basic styling applied

--- client/components/Dashboard/doctor/Form.js
+++ client/components/Dashboard/doctor/Form.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const Input = require('react-bootstrap').Input;
 const DocApiCard = require('./DocApiCard');
+const Button = require('react-bootstrap').Button;
 
 const AddForm = ({addDoc, searchDoc, docApiList}) => {
   return (
@@ -8,7 +9,7 @@ const AddForm = ({addDoc, searchDoc, docApiList}) => {
 	   <form onSubmit={searchDoc} className="form-horizontal">
 		    <Input name="firstname" type="text" label="First Name" labelClassName="col-xs-2" wrapperClassName="col-xs-10" placeholder="First Name" />
 		    <Input name="lastname" type="text" label="Last Name" labelClassName="col-xs-2" wrapperClassName="col-xs-10" placeholder="Last Name" />
-		    <button type="submit">Submit</button>
+		    <Button type="submit" bsStyle="primary" bsSize="large" block>Log in</Button>
 	   </form>
 	   <div>
 	   	{docApiList.map((doc) =>
