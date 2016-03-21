80300af3d86e1cf3b328f20469d2bf35306d5227
Huy Nguyen <hdnguyen2010@gmail.com>
2016-03-21 16:16:48 -0500

    hardcode profile structure

--- client/components/User/Profile.js
+++ client/components/User/Profile.js
@@ -1,28 +1,16 @@
 const React = require('react');
+const FormControls = require('react-bootstrap').FormControls.Static;
 
-const Todo = ({userInfo}) => {
-  return (
-    <div>
-      {userInfo}
-      <input ref={node => {
-        // console.log(node)
-        this.input = node;
-      }} />
-      <button onClick={() => {
-         store.dispatch({
-          type: 'ADD_TODO',
-          text: this.input.value,
-          id: nextTodoId++
-        });
-         this.input.value = '';
-      }}>
-        {userInfo}
-      </button>
-
-  
-
-    </div>
+const UserProfile = () => {
+  return(
+  <form className="form-horizontal">
+    <FormControls className="col-xs-10 col-xs-offset-2" value="I'm in a form" />
+    <FormControls label="Username" labelClassName="col-xs-2" wrapperClassName="col-xs-10" value="Billy" />
+    <FormControls label="Password" labelClassName="col-xs-2" wrapperClassName="col-xs-10" value="123" />
+    <FormControls label="Computer Mail" labelClassName="col-xs-2" wrapperClassName="col-xs-10" value="example@gmail.com" />
+    <FormControls label="Them 10-Digits" labelClassName="col-xs-2" wrapperClassName="col-xs-10">1234567890</FormControls>
+  </form>
   )
-}
+};
 
-module.exports = Todo;
+module.exports = UserProfile;
