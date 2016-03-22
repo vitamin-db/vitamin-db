f75b18f6086118d4c6a96aeffd17f3797c7750e4
Daniel Dinh <dtoand@yahoo.com>
2016-03-21 21:58:22 -0500

    twilio-mapping through various time works now

--- server/twilio/reminder.js
+++ server/twilio/reminder.js
@@ -4,7 +4,7 @@ const cronJob = require('cron').CronJob;
 const later = require('later');
 
 var numbers = [];
-var d = new Date('03/21/2016 21:36Z');
+var d = new Date('03/21/2016 21:56Z');
 
 var minute = later.minute.val(d);
 var hour = later.hour.val(d);
@@ -16,13 +16,13 @@ var arr = [];
 var parsedCron = '00 ' + minute + ' ' + hour + ' ' + day + ' * *';
 
 arr.push(parsedCron);
-arr.push('00 37 21 21 * *')
+arr.push('00 57 21 21 * *')
 console.log(arr)
 
 
 
-var Reminder = (arr) => {
-  arr.map((val) => 
+var Reminder = (array) => {
+  array.map((val) => 
   new cronJob(val, function(){
     for( var i = 0; i < numbers.length; i++ ) {
       client.sendMessage( { to: numbers[i], from: config.twilioNumber, body:'You have an upcoming Appointment!'}, function( err, data ) {
