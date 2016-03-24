76f29fae4bcdcfd8a891820620578a3f25b41fc1
Daniel Dinh <dtoand@yahoo.com>
2016-03-24 09:39:58 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    accountSid: 'ACa459550901c2e2f7a2754f51c76ffbbe',
-    authToken: '5241f349a4a9b3817b37dd08828b593e',
-    twilioNumber: '+15126451922'
+    accountSid: 'accountSid',
+    authToken: 'authToken',
+    twilioNumber: 'twilioNumber'
 };
