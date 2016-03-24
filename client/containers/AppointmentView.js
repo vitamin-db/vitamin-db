704ba03817b0ffac92f4136d02b909655f7d2adf
Daniel Dinh <dtoand@yahoo.com>
2016-03-24 00:58:57 -0500

    appointment view

--- client/containers/AppointmentView.js
+++ client/containers/AppointmentView.js
@@ -1,35 +1,46 @@
 // require packages
-const React       = require('react');
-const connect     = require('react-redux').connect;
+const React              = require('react');
+const connect            = require('react-redux').connect;
 // components/files
-const stateAction = require('../actionCreators/stateActions');
-const Schedule    = require('../components/Appointment/Schedule');
-const mock        = require('../model/mockData');
+const Schedule           = require('../components/Appointment/Schedule');
+const mock               = require('../model/mockData');
+// action files
+const stateAction        = require('../actionCreators/stateActions');
+const apptAction      = require('../actionCreators/appointmentActions');
 
-const Appointment = ({immunization, appointment}) => {
+
+const Appointment = ({states, dispatches}) => {
   return (
     <div>
-      <Schedule immunization={immunization} appointment={appointment}/>
+      <Schedule immunization={states.immunization} appointment={states.appointment} addAppt={dispatches.addAppointment}/>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
   return {
-    immunization: mock.Immunizations,
-    appointment: mock.Appointments
+    states: {
+      immunization: mock.Immunizations,
+      appointment: state.appoint
+    }
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-  	AddInformaton: (e) => {
-      e.preventDefault();
-      var onAdd = e.target.info.value;
-      var body = {info: onAdd}
-      dispatch(stateAction.AddInformation(body))
+    dispatches: {
+      addAppointment: (e) => {
+        e.preventDefault();
+        var body = {properties:{
+          date: e.target.date.value,
+          time: e.target.time.value
+        }};
+        console.log(e.target.date.value)
+        dispatch(apptAction.AddAppoint(body))
+      },
+
     }
-  }
+  };
 };
 
 var wrappedAppointment = connect(
