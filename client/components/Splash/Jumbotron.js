3b4763640f179937897e4018e67c9702d608224e
Daniel Dinh <dtoand@yahoo.com>
2016-03-28 14:57:58 -0500

    add upcoming shot date

--- client/components/Splash/Jumbotron.js
+++ client/components/Splash/Jumbotron.js
@@ -4,7 +4,7 @@ const Jumbotron = require('react-bootstrap').Jumbotron;
 const JumbotronInstance = () => (
   <div>
   <Jumbotron>
-    <h1>Hello, World!</h1>
+    <h1>Vitamin-DB!</h1>
     <p>Your personal health info management system</p>
   </Jumbotron>
   </div>
